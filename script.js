7407b576ca9ebfd3c6da2552ac25a484bdaf2051
Conor Maguire <maguiremconor@gmail.com>
2022-06-21 18:16:40 +0100

    removing

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("li").hover(function(){
-      $(this).css("background-color", " rgb(221, 178, 86)");
-      }, function(){
-      $(this).css("background-color", "white");
-    });
-  });
